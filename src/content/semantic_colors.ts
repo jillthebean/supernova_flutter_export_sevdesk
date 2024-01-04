3c688e16149b81a2bcf33a5cb13e61de01019d36
Jill Geri <Jill.Geri@sevdesk.de>
2024-01-04 11:23:33 +0100

    fix(SEVMOBILE-275): properly add themes

--- src/content/semantic_colors.ts
+++ src/content/semantic_colors.ts
@@ -1,32 +1,41 @@
-import { ColorToken, TokenType } from "@supernovaio/sdk-exporters";
+import { ColorToken, Token, TokenType } from "@supernovaio/sdk-exporters";
 import { ColorSchemeTemplateData } from "src/templates/color_scheme.template";
 import { ReferenceHelper, isPrimitive } from "./util";
 
 const className = "OffenburgColorScheme";
 
-export function createColors(helper: ReferenceHelper,): ColorSchemeTemplateData {
-    // Convert all color tokens to CSS variables
-    const colorTokens = helper.getUnprocessedTokensForType<ColorToken>(TokenType.color)
-    const colorVariables = semanticColors(colorTokens.filter(isPrimitive), helper)
+export function createColors(helper: ReferenceHelper, themeData: Record<string, Token[]>): ColorSchemeTemplateData {
+  // Convert all color tokens to CSS variables
+  const colorTokens = helper.getUnprocessedTokensForType<ColorToken>(TokenType.color)
+  const semanticColorTokens = colorTokens.filter(isPrimitive);
+  const colorVariables = semanticColors(semanticColorTokens, helper)
 
-    return {
-      className,
-      colors: Object.keys(colorVariables),
-      themes: {
-        "light": colorVariables
-      }
-    };
+  let themes = {
+    "light": colorVariables
+  }
+
+  for (var themeName in themeData) {
+    const tokens = themeData[themeName];
+    const themeColorTokens = tokens.filter(x => semanticColorTokens.some(y => y.id == x.id)) as ColorToken[];
+    themes[themeName] = semanticColors(themeColorTokens, helper);
+  }
+
+  return {
+    className,
+    colors: Object.keys(colorVariables),
+    themes,
+  };
 }
 
-function semanticColors(colorTokens: ColorToken[], helper: ReferenceHelper): Map<string,string> {
-    const variableNames = new Map<string, string>();
-    for (let token of colorTokens) {
-        const name = helper.resolveTokenVariableName(token, className);
+function semanticColors(colorTokens: ColorToken[], helper: ReferenceHelper): Map<string, string> {
+  const variableNames = new Map<string, string>();
+  for (let token of colorTokens) {
+    const name = helper.resolveTokenVariableName(token, className);
 
-        if (variableNames.has(name)) {
-            continue;
-        }
-        variableNames[name] = helper.resolveTokenReference(token.value.referencedTokenId!);
+    if (variableNames.has(name)) {
+      continue;
     }
-    return variableNames;
+    variableNames[name] = helper.resolveTokenReference(token.value.referencedTokenId!);
+  }
+  return variableNames;
 }
---
+++ src/token_export.ts
@@ -0,0 +1,100 @@
+import { FileHelper } from "@supernovaio/export-helpers"
+import { PulsarContext, RemoteVersionIdentifier, Supernova, Token, TokenGroup } from "@supernovaio/sdk-exporters"
+import { Eta } from "eta"
+import { createPrimitiveColors } from "./content/primitive_colors"
+import { createFontSizes, createGenericSpacings, createLetterSpacings, createLineHeights } from "./content/primitive_dimensions"
+import { createColors } from "./content/semantic_colors"
+import { createShadows } from "./content/shadows"
+import { createTypography } from "./content/typography"
+import { referenceHelper } from "./content/util"
+import { renderColorScheme } from "./templates/color_scheme.template"
+import { renderConstantsFile } from "./templates/constants.template"
+import { renderPrimitiveColors } from "./templates/primitive_colors.template"
+import { renderShadows } from "./templates/shadows.template"
+import { renderTypographyScheme } from "./templates/typography.template"
+
+export async function fetchTokenData(
+  sdk: Supernova,
+  context: PulsarContext,
+  remoteVersionIdentifier: RemoteVersionIdentifier,
+): Promise<[Token[], TokenGroup[], Record<string, Token[]>]> {
+  // Fetch the necessary data
+  let tokens = await sdk.tokens.getTokens(remoteVersionIdentifier)
+  let tokenGroups = await sdk.tokens.getTokenGroups(remoteVersionIdentifier)
+
+  // Filter by brand, if specified by the VSCode extension or pipeline configuration
+  if (context.brandId) {
+    tokens = tokens.filter((token) => token.brandId === context.brandId)
+    tokenGroups = tokenGroups.filter((tokenGroup) => tokenGroup.brandId === context.brandId)
+  }
+
+  // Apply theme, if specified by the VSCode extension or pipeline configuration
+  let themeTokens: Record<string, Token[]> = {}
+  const themes = await sdk.tokens.getTokenThemes(remoteVersionIdentifier)
+  for (const theme of themes) {
+    themeTokens[theme.codeName] = await sdk.tokens.computeTokensByApplyingThemes(tokens, [theme])
+  }
+  return [tokens, tokenGroups, themeTokens];
+}
+
+export function processTokenData(tokens: Token[], tokenGroups: TokenGroup[], themes: Record<string, Token[]>) {
+  const eta = new Eta({
+    debug: true,
+  });
+
+
+  const helper = referenceHelper(tokens, tokenGroups);
+
+  const primitiveTemplateData = createPrimitiveColors(helper);
+  const semanticsColorData = createColors(helper, themes);
+  const shadowsData = createShadows(helper);
+
+  const fontSizesData = createFontSizes(helper);
+  const lineHeightData = createLineHeights(helper);
+  const letterSpacingData = createLetterSpacings(helper);
+  const spacingData = createGenericSpacings(helper);
+
+  const typographyTemplateData = createTypography(helper);
+  return [
+    FileHelper.createTextFile({
+      relativePath: "./src/colors",
+      fileName: "primitive_colors.dart",
+      content: renderPrimitiveColors(eta, primitiveTemplateData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/colors",
+      fileName: "color_scheme.dart",
+      content: renderColorScheme(eta, semanticsColorData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/shadows",
+      fileName: "shadows.dart",
+      content: renderShadows(eta, shadowsData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/typography",
+      fileName: "line_heights.dart",
+      content: renderConstantsFile(eta, lineHeightData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/typography",
+      fileName: "font_sizes.dart",
+      content: renderConstantsFile(eta, fontSizesData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/typography",
+      fileName: "letter_spacings.dart",
+      content: renderConstantsFile(eta, letterSpacingData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/dimensions",
+      fileName: "spacings.dart",
+      content: renderConstantsFile(eta, spacingData),
+    }),
+    FileHelper.createTextFile({
+      relativePath: "./src/typography",
+      fileName: "typography.dart",
+      content: renderTypographyScheme(eta, typographyTemplateData),
+    }),
+  ]
+}

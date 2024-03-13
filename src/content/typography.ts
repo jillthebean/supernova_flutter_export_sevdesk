e2bccd31e9dbbcb01b2c9d2824e7a5260d312eac
Jill Geri <Jill.Geri@sevdesk.de>
2024-03-13 12:00:22 +0100

    fix(SEVMOBILE-435): calculate letter spacing correctly

--- src/content/typography.ts
+++ src/content/typography.ts
@@ -1,4 +1,4 @@
-import { FontSizeTokenValue, LetterSpacingTokenValue, LineHeightTokenValue, TokenType, TypographyToken, TypographyTokenValue } from "@supernovaio/sdk-exporters";
+import { FontSizeTokenValue, LetterSpacingTokenValue, LineHeightTokenValue, TokenType, TypographyToken, TypographyTokenValue, Unit } from "@supernovaio/sdk-exporters";
 import { FontFamilyTokenValue, FontWeightTokenValue, TextDecorationTokenValue } from "@supernovaio/sdk-exporters/build/sdk-typescript/src/model/tokens/SDKTokenValue";
 import { FontDescription, TypographyTemplateData } from "src/templates/typography.template";
 import { ReferenceHelper } from "./util";
@@ -26,24 +26,28 @@ function tokenToFontDescription(
   value: TypographyTokenValue,
   helper: ReferenceHelper
 ): FontDescription {
+  const fontSize = value.fontSize.measure;
   return {
-    lineHeight: mapMeasurementProp(value.lineHeight, helper),
-    fontSize: mapMeasurementProp(value.fontSize, helper),
+    lineHeight: mapMeasurementProp(value.lineHeight, helper, fontSize),
+    fontSize: mapMeasurementProp(value.fontSize, helper, fontSize),
     fontFamily: mapFontFamily(value.fontFamily, value.fontWeight, helper),
     fontWeight: mapFontWeight(value.fontWeight, helper),
-    letterSpacing: mapMeasurementProp(value.letterSpacing, helper),
+    letterSpacing: mapMeasurementProp(value.letterSpacing, helper, fontSize),
     decoration: mapFontDecorationProp(value.textDecoration, helper),
   };
 }
 
 type FontProp = LineHeightTokenValue | FontSizeTokenValue | LetterSpacingTokenValue;
 
-function mapMeasurementProp(prop: Pick<FontProp, "referencedTokenId" | "measure"> | null, helper: ReferenceHelper): string {
+function mapMeasurementProp(prop: Pick<FontProp, "referencedTokenId" | "measure" | "unit"> | null, helper: ReferenceHelper, fontSize: number): string {
   if (!prop) {
     return 'null';
   }
   if (prop.referencedTokenId) {
     return helper.resolveTokenReference(prop.referencedTokenId)
+  }
+  if (prop.unit == Unit.percent) {
+    return (fontSize / 100 * prop.measure).toFixed(2);
   }
   return `${prop.measure}`;
 }
